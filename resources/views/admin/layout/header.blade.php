fca3fa2affe6f48f35e2084b4035b530f49336f0
hungnp123 <105684834+hungnp123@users.noreply.github.com>
2022-08-06 19:10:22 +0700

    Update header.blade.php

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -32,5 +32,28 @@
     <nav class="navbar navbar-expand-lg bg-white">
         <a class="navbar-brand" href="homepage.php"><img style="margin: 15px;" src="img/weblogo.jpg" width="200px" height="55px"></a>
     </nav>
+    <div class="collapse navbar-collapse" id="myNavbar">
+						<ul class="nav navbar-nav">
+							<li><a href="homepage.php"><span class=" glyphicon glyphicon-home"> Home</a></li>
+								<li class="dropdown">
+									<a class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"> Account<span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="login">Login</a></li>
+										<li><a href="signup">Sign Up</a></li>
+										<li><a href="cart.php">Team management</a></li>
+									</ul>
+								</li>
+								<li><a href="managesong.php"><span class=" glyphicon glyphicon-music"> Match</a></li>
+								<li><a href="introduction.php"><span class=" glyphicon glyphicon-bookmark"> News</a></li>
+						</ul>
+							<ul class="nav navbar-nav navbar-right">
+								<div style="margin-top: 25px;" id="form_search">
+									<form method="get" action="search.php">
+										<input type="text" name="user_query" placeholder="Search a music" />
+										<input type="submit" name="Search" value="search" />
+									</form>
+								</div>	
+							</ul>
+					</div>
 </body>    
 </html>

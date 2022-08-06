9f43d81e3feb592db34be8f6c1188f7e45b6c1b1
hungnp123 <105684834+hungnp123@users.noreply.github.com>
2022-08-06 18:42:37 +0700

    by phi hung

---
+++ resources/views/admin/layout/header.blade.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home page</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<style type="text/css">
+    .header{
+    	
+    }
+    .header ul li{
+    	list-style: none;
+    }
+    .header img{
+    	float:left;
+    }
+	.header a{
+		margin-top: 15px;
+		margin-right: 10px;
+		margin-left: 10px;
+	}
+	.carousel-inner .item img {
+        margin: auto;
+        width: 1000px;
+        height: 500px;
+    }
+</style>
+<body>
+    <nav class="navbar navbar-expand-lg bg-white">
+        <a class="navbar-brand" href="homepage.php"><img style="margin: 15px;" src="img/logo.jpg" width="180px" height="50px"></a>
+    </nav>
+</body>    
+</html>

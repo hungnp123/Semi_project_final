89e673c21f3c92a5764a7fa0f5f63103ea901fc4
hungnp123 <105684834+hungnp123@users.noreply.github.com>
2022-08-07 23:09:57 +0700

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,25 +15,6 @@ class ProductController extends Controller
         return view('admin.product.index');
     }
 
-    public function create()
-    {
-        return view('admin.category.create');
-    } 
 
-    public function store(ProductFormRequest $request){
-        $validateData=$request->validated();
-        $product= new Product();
-        $product->name = $validateData['product_name'];
-        $product->price = $validateData['product_price'];
-        $category->description = $validateData['description'];
-        if($request->hasFile('product_img')){
-            $file =$request->file('product_img');
-            $ext=$file->getClientOriginalExtension();
-            $filename=time().'.'.$ext;
-
-            $file->move('upload/category/',$filename);
-            $category->image= $filename;
-        }
-    }    
 }
 

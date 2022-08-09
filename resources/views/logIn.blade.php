c207b3f41e3d6c9b2eb53321ad305e3f10b87240
LuongNgocChuan <105090596+LuongNgocChuan@users.noreply.github.com>
2022-08-09 08:43:21 +0700

    chuanedit

--- resources/views/logIn.blade.php
+++ resources/views/logIn.blade.php
@@ -90,7 +90,7 @@ span.su {
   </div>
    
   <div class="container">
-    <label for="uname"><b>Username</b></label>
+    <label for="uname"><b>Username:</b></label>
     <input type="text" placeholder="Enter Username" name="uname" required>
 
     <label for="psw"><b>Password</b></label>

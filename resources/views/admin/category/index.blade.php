9e32616288c1077ad07ca3cf225975082130395a
hungnp123 <105684834+hungnp123@users.noreply.github.com>
2022-08-12 07:48:54 +0700

    Update index.blade.php

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,1 +1,44 @@
-<h1> day là trang index category thôi <h1>
+
+<table class="table table-bordered">
+
+<tr>
+
+<th>No</th>
+
+<th>Name</th>
+
+<th>Details</th>
+
+<th width="280px">Action</th>
+
+</tr>
+
+@foreach($category as $cate_id => $category)
+
+<tr>
+
+<td>{{$cate_id+1}}</td>
+
+<td>{{ $category->name }}</td>
+
+<td>{{ $category->description }}</td>
+
+<td>
+
+<form action="{{ route('category.destroy',$category->id) }}" method="POST">
+
+<a class="btn btn-info" href="{{ route('category.show',$category->id) }}">Show</a>
+
+<a class="btn btn-primary" href="{{ route('category.edit',$category->id) }}">Edit</a>
+
+<a class="btn btn-primary" href="{{ route('category.destroy',$category->id) }}">Delete</a>
+
+</form>
+
+</td>
+
+</tr>
+
+@endforeach
+
+</table>

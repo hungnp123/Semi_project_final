d18e8ba55a9cfba00d0fb88e58cc4a7cc2c680c0
hungnp123 <105684834+hungnp123@users.noreply.github.com>
2022-08-24 14:07:45 +0700

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,16 +6,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth; 
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use App\Models\Product;
 
 class LoginController extends Controller
 {
+    
     public function getLogin (){
         return view ('login'); 
     }
     public function postLogin (Request $request) {
         $arr = ['user_name' => $request->user_name, 'password' => $request -> password]; 
         if(Auth::attempt($arr)) {
-            return view ('user/homepage'); 
+            $data = Product::paginate(8);
+            return view('user.homepage',['product' => $data]);
         }else {
             dd('Failed');
         }

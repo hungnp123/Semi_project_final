5e6eb82f519b6e0399a610fad5dad12f685c8dde
hungnp123 <105684834+hungnp123@users.noreply.github.com>
2022-08-24 20:46:03 +0700

    Create menubar.blade.php

---
+++ resources/views/admin/layout/menubar.blade.php
@@ -0,0 +1,16 @@
+<div class="menu">
+			<ul class="navbar-nav">
+				<li><a href="{{route('admin')}}"><span class=" glyphicon glyphicon-home"> Home</a></li>
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-bookmark"> Category<span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="user/login">Login</a></li>
+								<li><a href="user/signup">Sign Up</a></li>
+							</ul>
+				    </li>
+				<li><a href="/product/index"><span class=" glyphicon glyphicon-book"> Books</a></li>
+			</ul>
+		</div>			
+		</div>
+    </nav>
+	</div>    

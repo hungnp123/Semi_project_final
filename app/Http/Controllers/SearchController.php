f4be5e722b4b8bdfcfa0580f2152e55058af7025
hungnp123 <105684834+hungnp123@users.noreply.github.com>
2022-08-24 13:46:28 +0700

    search edit

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 class SearchController extends Controller
 {
     public function BookSearch(Request $request) {
-        $data = Product::paginate(8);
+        $data = Product::all();
         $search=$request->input('product_name'); 
         $products=Product::where('product_name', 'LIKE', '%'.$search.'%') ->get();
         return view('user.search',['product' => $data],compact('products'));

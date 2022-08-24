e00f928ec95940678a24b4baf8135402e81571ab
Le Hoang Minh <minhlhbh00058@fpt.edu.vn>
2022-08-24 12:39:57 +0700

    href fixed

--- resources/views/user/layout/menubar.blade.php
+++ resources/views/user/layout/menubar.blade.php
@@ -12,8 +12,8 @@
 					<li class="dropdown">
 						<a class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-bookmark"> Category<span class="caret"></span></a>
 							<ul class="dropdown-menu">
-								<li><a href="login">Login</a></li>
-								<li><a href="signup">Sign Up</a></li>
+								<li><a href="user/login">Login</a></li>
+								<li><a href="user/signup">Sign Up</a></li>
 							</ul>
 					</li>
 					<li><a href="/product/index"><span class=" glyphicon glyphicon-book"> Books</a></li>
